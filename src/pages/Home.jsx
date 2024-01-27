7771572a1fbf28f0903377d09b7528170a2bac7f
Onyami George William <Imayno85@gmail.com>
2024-01-27 18:40:09 +0300

    imported_sky_background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import Loader from "../components/Loader";
 
 import Island from "../models/Island";
+import Sky from "../models/Sky";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"></div> */
@@ -32,11 +33,15 @@ const Home = () => {
       >
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <ambientLight intensity={0.5} />
 
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
